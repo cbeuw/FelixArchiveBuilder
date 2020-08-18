6cb73456cf6bb2163d2632d33b7e44814e1d081f
Andy Wang <cbeuw.andy@gmail.com>
2020-08-18 23:43:11 +0100

    PDF extractor

---
+++ extract_text.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# pdftotext is a part of Xpdf command line tools: https://www.xpdfreader.com/download.html
+
+function extract_issue() {
+  issue_name=felix_$1
+  if [ ! -e "$pdf_root/$issue_name.pdf" ]; then
+    return 1
+  fi
+  dest="$output_dir/$1/"
+  mkdir -p $dest
+  pdftotext -raw $issue_name.pdf $dest/$issue_name.txt
+
+  pushd $dest || exit
+  awk '{i++}length {print > i".txt"}' RS='\f' $issue_name.txt
+  rm -f $issue_name.txt
+  popd || exit
+}
+
+function extract_range(){
+  start=$1
+  finish=$2
+  for issue in $(seq $start $finish); do
+    extract_issue $issue;
+  done
+}
+
+pdf_root="${1:-./}"
+output_dir="${2:-text/}"
+
+extract_range 1 1737
